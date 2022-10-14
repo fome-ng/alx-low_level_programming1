34169daee9affa68d49f869db41225c5ffe4b7d4
fome-ng <fomemede@gmail.com>
2022-10-14 16:47:57 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -> assign a random number
+ * and print the last digit each time
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+	printf("%d", n);
+}
+printf("\n");
+return (0);
+}
+
+
