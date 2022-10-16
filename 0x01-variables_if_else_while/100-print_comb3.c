df1a3e7d33da56110db6c51888aadd55e9b6d228
fome-ng <fomemede@gmail.com>
2022-10-15 21:59:22 -0700

    100 print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints all possible different combinations of two digits.
+ * main - Prints all possible different combinations of two digits.
  *
  * Return: Always 0.
  */
@@ -8,9 +8,9 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit 1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit 1 + 1; digit 2 < 10; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
